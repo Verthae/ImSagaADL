05651409256ff112fab323d06584a30afd2e79ad
Haledire <m_craftsman@hotmail.com>
2021-08-04 21:00:24 -0400

    Commiting from PyCharm

---
+++ imsaga_dl.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+
+chara_url = '{url}resources/charaPtn/{charaID}/chara.{typed}'
+spine_format = '{url}resources/character/{charaID}/battlewait.{typed}'
+types = ['png', 'atlas', 'json']
+card_url = '{url}resources/{path}/{charaID}.png'
+sources = ['charaCard/chara', 'charaCard/card', 'charaFace', 'charaFaceImg']
+list_character = '{url}resources/parameter/list_character.json'
+
+os.makedirs('chara', exist_ok=True)
+os.makedirs('sound', exist_ok=True)
+
+if __name__ == '__main__':
+    urlBase = json.loads(requests.get('https://game.eclipse.imperialsaga.jp/na/config.txt').content)['resource']
+    charaJson = json.loads(requests.get(list_character.format(url=urlBase)).content)
+    charaList = [x['charaPtn'] for x in charaJson]
+
+    for chara in charaList:
+        # charaUrl
+        os.makedirs(f'chara/{chara}', exist_ok=True)
+        for filetype in types[:1]:
+            with open(f'chara/{chara}/{chara}_sprite.{filetype}') as file:
+                file.write(requests.get(chara_url.format(url=urlBase, charaID=chara, typed=filetype)))
+        # spineUrl
+        for filetype in types:
+            with open(f'chara/{chara}/{chara}_battlewait.{filetype}') as file:
+                file.write(requests.get(spine_format.format(url=urlBase, charaID=chara, typed=filetype)))
+        # card_url
+        for source in sources:
+            with open(f'chara/{chara}_{source.split("/")[-1]}/{chara}.png') as file:
+                file.write(requests.get(card_url.format(url=urlBase, path=source, charaID=chara)))
+
