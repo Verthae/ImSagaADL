8c4c9ead23ff9b6d49949ccaac051492e2fae254
Haledire <m_craftsman@hotmail.com>
2021-08-04 21:48:30 -0400

    fixing directory mistake

--- imsaga_dl.py
+++ imsaga_dl.py
@@ -9,26 +9,39 @@ card_url = '{url}resources/{path}/{charaID}.png'
 sources = ['charaCard/chara', 'charaCard/card', 'charaFace', 'charaFaceImg']
 list_character = '{url}resources/parameter/list_character.json'
 
-os.makedirs('chara', exist_ok=True)
-os.makedirs('sound', exist_ok=True)
+ROOT = os.path.dirname(os.path.realpath(__file__))
+os.makedirs(os.path.join(ROOT, 'chara'), exist_ok=True)
+os.makedirs(os.path.join(ROOT, 'sound'), exist_ok=True)
 
 if __name__ == '__main__':
     urlBase = json.loads(requests.get('https://game.eclipse.imperialsaga.jp/na/config.txt').content)['resource']
     charaJson = json.loads(requests.get(list_character.format(url=urlBase)).content)
     charaList = [x['charaPtn'] for x in charaJson]
 
-    for chara in charaList:
+    # for chara in charaList[1:]:
+    #     os.makedirs(os.path.join(ROOT, f'chara/{chara}'))
+
+
+    for index, chara in enumerate(charaList):
+        print(f'Downloading {chara}')
         # charaUrl
-        os.makedirs(f'chara/{chara}', exist_ok=True)
-        for filetype in types[:1]:
-            with open(f'chara/{chara}/{chara}_sprite.{filetype}') as file:
-                file.write(requests.get(chara_url.format(url=urlBase, charaID=chara, typed=filetype)))
+        dl_location = os.path.join(ROOT, f'chara/{chara}')
+        os.makedirs(dl_location, exist_ok=True)
+        for filetype in types[:2]:
+            print(f'chara/{chara}/{chara}_sprite.{filetype}')
+            dl_file = requests.get(chara_url.format(url=urlBase, charaID=chara, typed=filetype))
+            if dl_file.status_code == 200:
+                with open(f'chara/{chara}/{chara}_sprite.{filetype}', 'wb') as file:
+                    file.write(dl_file.content)
         # spineUrl
-        for filetype in types:
-            with open(f'chara/{chara}/{chara}_battlewait.{filetype}') as file:
-                file.write(requests.get(spine_format.format(url=urlBase, charaID=chara, typed=filetype)))
-        # card_url
-        for source in sources:
-            with open(f'chara/{chara}_{source.split("/")[-1]}/{chara}.png') as file:
-                file.write(requests.get(card_url.format(url=urlBase, path=source, charaID=chara)))
+        if index > 0:
+            for filetype in types:
+                dl_file = requests.get(spine_format.format(url=urlBase, charaID=chara, typed=filetype))
+                if dl_file.status_code == 200:
+                    with open(f'chara/{chara}/{chara}_battlewait.{filetype}', 'wb') as file:
+                        file.write(dl_file.content)
+            # card_url
+            for source in sources:
+                with open(f'chara/{chara}/{chara}_{source.split("/")[-1]}.png', 'wb') as file:
+                    file.write(requests.get(card_url.format(url=urlBase, path=source, charaID=chara)).content)
 
